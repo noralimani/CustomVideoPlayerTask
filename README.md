7aa9cc530194d0e1a7119276bb24834c4ae28108
Nora Limani <nora.limani-non-empl@moodys.com>
2022-10-12 11:09:13 +0200

    custom video player for tractive

---
+++ README.md
@@ -0,0 +1,11 @@
+## Custom Video Player
+
+Custom video player using the HTML5 video element and JavaScript with a custom design
+
+## Task Specifications
+
+- Display custom video player styled with CSS
+- Play/pause button
+- Play/pause on video
+- timestamp
+- integrated wave sound
---
+++ js/script.js
@@ -0,0 +1,50 @@
+let video = document.getElementById("tractiveVideo");
+let playBtn = document.getElementById("playButton");
+let audioBtn = document
+  .getElementById("shadowPlayer")
+  .shadowRoot.getElementById("play");
+let volumeSlider = document.getElementById("volumeSlider");
+const timestamp = document.getElementById("timestamp");
+let currentTime = 0;
+
+// update the video status
+function toggleVideoStatus() {
+  video.paused ? video.play() : video.pause();
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+  video.paused
+    ? (playBtn.innerHTML = '<i class="fa-solid fa-play"></i>')
+    : (playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>');
+}
+
+//timestamp
+function updateProgress() {
+  currentTime = (video.currentTime / video.duration) * 100;
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+//event listeners
+video.addEventListener("click", () => {
+  toggleVideoStatus();
+  audioBtn.click();
+});
+playBtn.addEventListener("click", () => {
+  toggleVideoStatus();
+  audioBtn.click();
+});
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
